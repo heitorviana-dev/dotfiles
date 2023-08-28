50cb79232b2f8d783d14e95f22ca8f5972c8f92d
3rfaan <28968837+3rfaan@users.noreply.github.com>
2023-08-28 22:05:48 +0200

    Small optical code improvements

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ normal=$(tput sgr0)
 bold=$(tput bold)
 
 red=$(tput setaf 1)
-grn=$(tput setaf 2)
-ylw=$(tput setaf 3)
-gry=$(tput setaf 8)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+grey=$(tput setaf 8)
 
 # Command line arguments
-system=$1
+vb=$1
 
 # Directories
 CONFIG_DIR=$HOME/.config
 DOWNLOAD_DIR=$HOME/Downloads
 
 # Header
-printf "${gry}%s${normal}\n${bold}${grn}%s${normal}\n${bold}${ylw}%s${normal}\n${ylw}%s${normal}\n${gry}%s${normal}\n\n" "
+printf "${grey}%s${normal}\n${bold}${green}%s${normal}\n${bold}${yellow}%s${normal}\n${yellow}%s${normal}\n${grey}%s${normal}\n\n" "
 
 ███████╗██╗   ██╗███████╗██████╗ ███████╗ ██████╗ ██████╗ ███████╗███████╗████████╗
 ██╔════╝██║   ██║██╔════╝██╔══██╗██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝╚══██╔══╝
@@ -36,7 +36,7 @@ printf "${gry}%s${normal}\n${bold}${grn}%s${normal}\n${bold}${ylw}%s${normal}\n$
 function main() {
 	while true; do
 		echo -e "${bold}${red}===> This installer will delete ALL files in your ~/.config directory.${normal}"
-		read -r -p "${ylw}Do you want to proceed? [y/N] ${normal}" yn
+		read -r -p "${yellow}Do you want to proceed? [y/N] ${normal}" yn
 
 		case $yn in
 		[Yy]*)
@@ -48,7 +48,7 @@ function main() {
 			install_shell_tools
 			remove_install_file
 
-			echo "${bold}${grn}Installation succeeded! Exiting...${normal}"
+			echo "${bold}${green}Installation succeeded! Exiting...${normal}"
 			exit 0
 			;;
 		[Nn]* | "")
@@ -63,38 +63,38 @@ function main() {
 # Checking if ~/.config already exists and wipe it if it does
 function pre_install_checks() {
 	if [[ -d "$CONFIG_DIR" ]]; then
-		echo "${ylw}$CONFIG_DIR does already exist, deleting all files...${normal}"
+		echo "${yellow}$CONFIG_DIR does already exist, deleting all files...${normal}"
 		rm -rfv "$CONFIG_DIR"
 	else
-		echo "${ylw}Creating new config directory ($CONFIG_DIR)${normal}"
+		echo "${yellow}Creating new config directory ($CONFIG_DIR)${normal}"
 	fi
 
 	# Creating new empty ~/.config
-	echo "${ylw}Creating new $CONFIG_DIR directory...${normal}"
+	echo "${yellow}Creating new $CONFIG_DIR directory...${normal}"
 	mkdir -p "$CONFIG_DIR" || {
 		echo "${bold}${red}Failed creating ${CONFIG_DIR}, exiting...${normal}"
 		exit 1
 	}
-	echo "${ylw}Done!${normal}"
+	echo "${yellow}Done!${normal}"
 
 	# Refreshing XDG user directories
-	echo "${ylw}Creating XDG user directories...${normal}"
+	echo "${yellow}Creating XDG user directories...${normal}"
 	xdg-user-dirs-update || {
 		echo "${bold}${red}Failed creating XDG user directories, exiting...${normal}"
 		exit 1
 	}
-	echo "${ylw}Done!${normal}"
+	echo "${yellow}Done!${normal}"
 }
 
 # Removing .git directory so it won't get copied unnecessary
 function remove_git_directory() {
-	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Removing .git directory...${normal}"
-		rm -rf .git || {
+	if [[ -d $DOWNLOAD_DIR/dotfiles/.git ]]; then
+		echo "${yellow}Removing .git directory...${normal}"
+		rm -Rf "$DOWNLOAD_DIR/dotfiles/.git" || {
 			echo "${bold}${red}Failed removing .git directory, exiting...${normal}"
 			exit 1
 		}
-		echo "${ylw}Done!${normal}"
+		echo "${yellow}Done!${normal}"
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
 		exit 1
@@ -104,12 +104,12 @@ function remove_git_directory() {
 # Copying dotfiles from Downloads folder to ~/.config
 function copy_config_files() {
 	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}...${normal}"
+		echo "${yellow}Copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}...${normal}"
 		cp -Rfv "$DOWNLOAD_DIR/dotfiles/." "$CONFIG_DIR" || {
 			echo "${bold}${red}Failed copying files from ${DOWNLOAD_DIR} to ${CONFIG_DIR}, exiting...${normal}"
 			exit 1
 		}
-		echo "${ylw}Done!${normal}"
+		echo "${yellow}Done!${normal}"
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
 		exit 1
@@ -119,17 +119,17 @@ function copy_config_files() {
 # Copying files which target destinations are not in ~/.config
 function copy_others() {
 	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Copying files to other directories${normal}"
+		echo "${yellow}Copying files to other directories${normal}"
 
 		# If user provided command line argument ("utm" or "vbox") then copy X11 files to xorg.conf.d else don't copy anything to xorg.conf.d
-		if [ "$system" == "utm" ]; then
+		if [ "$vb" == "utm" ]; then
 			sudo cp -Rfv "$DOWNLOAD_DIR/dotfiles/Xorg/UTM/." "/etc/X11/xorg.conf.d"
-		elif [ "$system" == "vbox" ]; then
+		elif [ "$vb" == "vbox" ]; then
 			sudo cp -Rfv "$DOWNLOAD_DIR/dotfiles/Xorg/VBox/." "/etc/X11/xorg.conf.d"
 		fi
 
         # If no argument was provided delete Xorg folder from ~/.config
-        if [ -z "$system" ] && [ -d "$CONFIG_DIR/Xorg" ]; then
+        if [ -z "$vb" ] && [ -d "$CONFIG_DIR/Xorg" ]; then
             sudo rm -Rfv "$CONFIG_DIR/Xorg"
         fi
 
@@ -140,7 +140,7 @@ function copy_others() {
 
 		cp -Rfv "$DOWNLOAD_DIR/dotfiles/Xorg/.xinitrc" "$HOME/.xinitrc"
 		cp -Rfv "$DOWNLOAD_DIR/dotfiles/zsh/.zshrc" "$HOME/.zshrc"
-		echo "${ylw}Done!${normal}"
+		echo "${yellow}Done!${normal}"
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
 		exit 1
@@ -150,20 +150,20 @@ function copy_others() {
 # Copying wallpaper to Downloads folder
 function set_wallpaper() {
 	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Setting wallpaper${normal}"
+		echo "${yellow}Setting wallpaper${normal}"
 		cp -Rfv "$DOWNLOAD_DIR/dotfiles/flowers.png" "$DOWNLOAD_DIR/flowers.png"
-		echo "${ylw}Done!${normal}"
+		echo "${yellow}Done!${normal}"
 
 		# Changing directory to ~/.config
 		cd "$CONFIG_DIR" || exit 1
-		echo "${ylw}Changed directory to $(pwd)${normal}"
+		echo "${yellow}Changed directory to $(pwd)${normal}"
 
-		echo "${ylw}Removing some unnecessary files...${normal}"
+		echo "${yellow}Removing some unnecessary files...${normal}"
 		rm -rfv asus Xorg urxvt lf i3/i3status screenshot-*.png nvim/init.vim nvim/coc-settings.json install.sh zsh || {
 			echo "${bold}${red}Failed removing directories not needed for the configuration, exiting..."
 			exit 1
 		}
-		echo "${ylw}Done!${normal}"
+		echo "${yellow}Done!${normal}"
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
 		exit 1
@@ -172,34 +172,34 @@ function set_wallpaper() {
 
 function install_shell_tools() {
 	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Installing shell tools${normal}"
+		echo "${yellow}Installing shell tools${normal}"
 
 		# Packer
 		if [[ ! -d $HOME/.local/share/nvim/site/pack/packer ]]; then
 			git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 		else
-			echo "${grn}Packer is already installed${normal}"
+			echo "${green}Packer is already installed${normal}"
 		fi
 
 		# ZSH auto suggestions
 		if [[ ! -d $HOME/.zsh/zsh-autosuggestions ]]; then
 			git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
 		else
-			echo "${grn}ZSH auto suggestions is already installed${normal}"
+			echo "${green}ZSH auto suggestions is already installed${normal}"
 		fi
 
 		# ZSH syntax highlighting
 		if [[ ! -d $HOME/.zsh/zsh-syntax-highlighting ]]; then
 			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/zsh-syntax-highlighting"
 		else
-			echo "${grn}ZSH syntax highlighting is already installed${normal}"
+			echo "${green}ZSH syntax highlighting is already installed${normal}"
 		fi
 
 		# Icons for ranger
 		if [[ ! -d $CONFIG_DIR/ranger/plugins/ranger_devicons ]]; then
 			git clone https://github.com/alexanderjeurissen/ranger_devicons "$CONFIG_DIR/ranger/plugins/ranger_devicons"
 		else
-			echo "${grn}Ranger devicons is already installed${normal}"
+			echo "${green}Ranger devicons is already installed${normal}"
 		fi
 	else
 		echo "${bold}${red}Make sure you cloned (https://github.com/3rfaan/dotfiles.git) into your ~/Downloads folder!${normal}"
@@ -210,9 +210,9 @@ function install_shell_tools() {
 # Removing install files after moving them to .config
 function remove_install_file() {
 	if [[ -d $DOWNLOAD_DIR/dotfiles ]]; then
-		echo "${ylw}Removing installation files...${normal}"
+		echo "${yellow}Removing installation files...${normal}"
 		rm -rf "$DOWNLOAD_DIR/dotfiles"
-		echo "${ylw}Done removing installation files${normal}"
+		echo "${yellow}Done removing installation files${normal}"
 	fi
 }
 
